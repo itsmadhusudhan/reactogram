1a75e606a5f7db82c236c82248b2523bc3850144
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-07-16 22:23:17 +0530

    adding header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import * as styles from "./Header.scss"
+import React from "react";
+import PropTypes from "prop-types";
+import * as styles from "./Header.scss";
+import { Link } from "react-router-dom";
+import instagram from "../../images/instagram.svg";
 
-const Header=(props)=>{
-  return(
+console.log(instagram);
+const Header = props => {
+  return (
     <header className={styles.header}>
-      header here
+      <img src={instagram} className={styles.logo} />
+      <h1>Instagram</h1>
+      <nav className={styles.menu}>
+        <input type="text" placeholder="search" />
+        <Link to="/" className={styles.menu__item}>
+          Explore
+        </Link>
+        <Link to="/" className={styles.menu__item}>
+          Heart
+        </Link>
+        <Link to="/" className={styles.menu__item}>
+          Profile
+        </Link>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/HomeContainer/HomeContainer.jsx
+++ src/components/HomeContainer/HomeContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as styles from './HomeContainer.scss';
+import instagram from "../../images/instagram.svg";
 
 const HomeContainer=(props)=>{
   return(
     <div className={styles.home__container}>
      Home container
+     <img src={instagram} className={styles.logo} />
+
      </div>
   )
 }
